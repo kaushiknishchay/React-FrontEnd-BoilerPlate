364e3da26d6804076910b1009ffe415be5963414
Nishchay <kaushiknishchay@gmail.com>
2018-03-23 12:10:12 +0530

    added coveralls embed code in ReadME

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+export default () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <p className="App-intro">
+                To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>
+);
